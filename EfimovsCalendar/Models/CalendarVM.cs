91ac513186d8b86960eb39badafa512928f354e6
IEEfimov <vanchick.fimchick15@gmail.com>
2018-10-30 13:26:09 +0300

    The CalendarVM creation is starting

---
+++ EfimovsCalendar/Models/CalendarVM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EfimovsCalendar.Models
+{
+    public class CalendarVM
+    {
+        public struct DayMonth
+        {
+            int month;
+            int year;
+
+            public DayMonth(int month, int year)
+            {
+                this.month = month;
+                this.year = year;
+            }
+
+            public int Month { get => month;}
+            public int Year { get => year;}
+        }
+
+        int currentMonth;
+        int currentYaer;
+        string monthName;
+
+        public DayMonth nextMonth
+        {
+            get
+            {
+                DayMonth temp = new DayMonth();
+                return temp;
+            }
+        }
+    }
+
+    
+}
